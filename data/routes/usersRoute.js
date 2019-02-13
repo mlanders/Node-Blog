40270fe7db73fbfbf23e55deef3ce24df01bf009
Mike Landers <landers.mike@gmail.com>
2019-02-13 15:14:10 -0800

    DELETE user Uppercase middleware

--- data/routes/usersRoute.js
+++ data/routes/usersRoute.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const usersdb = require('../helpers/userDb');
+const postdb = require('../helpers/postDb');
 const router = express.Router();
+const { uppercase } = require('../middleware/uppercase');
 
 // Create new user
-router.post('/', async (req, res) => {
+router.post('/', uppercase, async (req, res) => {
 	const { name } = req.body;
 	const newUser = { name };
+	console.log('newuser', newUser);
 
 	try {
 		const userId = await usersdb.insert(newUser);
@@ -27,11 +30,15 @@ router.get('/', async (req, res) => {
 
 // Get specific user with ID
 router.get('/:id', async (req, res) => {
+	const userId = await usersdb.getById(req.params.id);
 	try {
-		const userId = await usersdb.getById(req.params.id);
-		res.status(200).json({ success: true, userId });
+		if (userID) {
+			res.status(200).json({ success: true, userId });
+		} else {
+			res.status(404).json({ success: false, message: 'unable to find that user' });
+		}
 	} catch {
-		res.status(500).json({ success: false, message: `can\'t get users` });
+		res.status(500).json({ success: false, message: `User not valid` });
 	}
 });
 
@@ -52,7 +59,6 @@ router.put('/:id', async (req, res) => {
 	const changes = { name };
 	try {
 		const user = await usersdb.getById(id);
-		console.log(user);
 		if (user) {
 			const updatedUser = await usersdb.update(id, changes);
 			res.status(200).json({ success: true, message: `user updated`, updatedUser });
@@ -65,21 +71,27 @@ router.put('/:id', async (req, res) => {
 });
 
 // Delete user
-router.put('/:id', async (req, res) => {
-	const { id } = req.params;
-	const { name } = req.body;
-	const changes = { name };
+router.delete('/:id', async (req, res) => {
 	try {
+		const { id } = req.params;
 		const user = await usersdb.getById(id);
-		console.log(user);
-		if (user) {
-			const updatedUser = await usersdb.update(id, changes);
-			res.status(200).json({ success: true, message: `user updated`, updatedUser });
+		const userPosts = await usersdb.getUserPosts(id);
+		if (userPosts.length > 0) {
+			for (let i = 0; i < userPosts.length; i++) {
+				await postdb.remove(userPosts[i].id);
+			}
+			const deletedUser = await usersdb.remove(id);
+			res.status(200).json({ success: true, message: `user deleted` });
 		} else {
-			res.status(404).json({ success: false, message: 'User not found' });
+			if (user) {
+				const deletedUser = await usersdb.remove(id);
+				res.status(200).json({ success: true, message: `user deleted` });
+			} else {
+				res.status(404).json({ success: false, message: 'User not found' });
+			}
 		}
 	} catch {
-		res.status(500).json({ success: false });
+		res.status(500).json({ success: false, message: `unable to delete user` });
 	}
 });
 
