fcdf6851d0f98540a7c7cde899ab78c7f7d17581
Mike Landers <landers.mike@gmail.com>
2019-02-14 12:20:10 -0800

    typo with require dotenv

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const ('dotenv').config();
+require('dotenv').config();
 const server = require('./server');
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
 	console.log(`Server is running on port 5000`);
 });
